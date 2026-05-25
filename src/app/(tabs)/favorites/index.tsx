5e738979eb71de1b083dc5131009bc4644e63364
Christopher J Baker <christopher@christopherjbaker.dev>
2026-05-25 18:34:50 +0200

    design system

--- src/app/(tabs)/favorites/index.tsx
+++ src/app/(tabs)/favorites/index.tsx
@@ -1,5 +1,7 @@
-import { Link, Stack } from "expo-router"
-import { StyleSheet, Text, View } from "react-native"
+import { Stack } from "expo-router"
+import { StyleSheet, View } from "react-native"
+
+import Typography from "#design/elements/Typegraphy"
 
 const App: React.FC = () => {
   return (
@@ -7,11 +9,11 @@ const App: React.FC = () => {
       <Stack.Screen options={{ title: "Favorites" }} />
 
       <View style={styles.container}>
-        <Text>Favorites</Text>
+        <Typography variant="title">Favorites</Typography>
 
-        <Link href="/favorites/one">One</Link>
-        <Link href="/favorites/two">Two</Link>
-        <Link href="/favorites/three">Three</Link>
+        <Typography href="/favorites/one">One</Typography>
+        <Typography href="/favorites/two">Two</Typography>
+        <Typography href="/favorites/three">Three</Typography>
       </View>
     </>
   )
