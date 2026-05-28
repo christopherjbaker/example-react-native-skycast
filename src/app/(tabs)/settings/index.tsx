2bb9d9c3e41c42cb8992d0c21b178f3ea8c0c920
Christopher J Baker <christopher@christopherjbaker.dev>
2026-05-28 15:34:24 +0200

    add form-group, text-field, toggle-field

--- src/app/(tabs)/settings/index.tsx
+++ src/app/(tabs)/settings/index.tsx
@@ -1,12 +1,21 @@
 import { StyleSheet, View } from "react-native"
 
+import TextField from "#design/elements/fields/Text"
 import Typography from "#design/elements/Typegraphy"
+import FormGroup from "#design/elements/FormGroup"
+import { useState } from "react"
 
 const App: React.FC = () => {
+  const [name, setName] = useState("Home")
+
   return (
     <>
       <View style={styles.container}>
         <Typography variant="title">Settings</Typography>
+
+        <FormGroup label="Home Name">
+          <TextField onChange={setName} value={name} />
+        </FormGroup>
       </View>
     </>
   )
--- src/app/(tabs)/settings/notifications.tsx
+++ src/app/(tabs)/settings/notifications.tsx
@@ -1,24 +1,42 @@
-import { useEffect } from "react"
-import { StyleSheet, View } from "react-native"
+import { Button, StyleSheet, View } from "react-native"
 
 import Typography from "#design/elements/Typegraphy"
 import { createNotification } from "#shared/notifications"
+import FormGroup from "#design/elements/FormGroup"
+import ToggleField from "#design/elements/fields/Toggle"
+import { useState } from "react"
 
 const App: React.FC = () => {
-  useEffect(() => {
-    setTimeout(async () => {
-      await createNotification({
-        title: "Full Title",
-        short: "Short",
-        body: "Exercitation nulla pariatur minim proident ullamco et pariatur.",
-      })
-    }, 2500)
-  }, [])
+  const [enabled, setEnabled] = useState(false)
+  const [favorites, setFavorites] = useState(false)
 
   return (
     <>
       <View style={styles.container}>
         <Typography variant="title">Notifications</Typography>
+
+        <FormGroup label="Enable Notifications">
+          <ToggleField onChange={setEnabled} value={enabled} />
+        </FormGroup>
+
+        {enabled && (
+          <>
+            <FormGroup label="Favorites Warnings">
+              <ToggleField onChange={setFavorites} value={favorites} />
+            </FormGroup>
+
+            <Button
+              title="Test Notification"
+              onPress={() =>
+                createNotification({
+                  title: "Full Title",
+                  short: "Short",
+                  body: "Exercitation nulla pariatur minim proident ullamco et pariatur.",
+                })
+              }
+            />
+          </>
+        )}
       </View>
     </>
   )
